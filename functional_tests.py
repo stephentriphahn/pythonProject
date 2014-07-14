c8282fa00c3fdf41ac34ba05e2f9b14c440bccb6
steve <stephentriphahn@yahoo.com>
2014-07-13 20:20:31 -0400

    functional test now looks for to do list entries

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest 
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,7 +16,32 @@ class NewVisitorTest(unittest.TestCase):
 
 		#title/header check
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		#user is invited to add list item right away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+		
+		#user typs buy peacock feathers as list item
+		inputbox.send_keys('Buy Peacock Feathers')
+
+		#user hits enter, page reloads and displays new item on list
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy Peacock Feathers' for row in rows)
+		)
+
 		self.fail('Finish the test!')
+
+
+
 
 if __name__=='__main__':
 	unittest.main(warnings='ignore')
